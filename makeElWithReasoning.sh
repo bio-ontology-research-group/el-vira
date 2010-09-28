b2e5be3ce5c6b0511820410364f94dff08d5cf4d
leechuck2 <leechuck2@66c8a2c3-1157-fc15-5f84-f21a2d9a3beb>
2010-09-28 20:27:14 +0000

    fixed command line interface

--- makeElWithReasoning.sh
+++ makeElWithReasoning.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
-export tempcp=`dirname $0`/elvira.jar
+export tempcp=elvira.jar
 for i in lib/*jar; do tempcp="$tempcp:$i"; done
-echo $tempcp
 java -Xmx2048M -Xms250M \
      -cp $tempcp \
-    de.bioonto.elvira.MakeElWithReasoning $1 $2 $3 $4 $5
+    de.bioonto.elvira.MakeElWithReasoning $1 $2 $3 $4 $5 $6 $7 $8 $9
